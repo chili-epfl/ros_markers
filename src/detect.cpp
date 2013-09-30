10b1acacd449db6078a514ae3335f62dc0c752e2
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2013-09-30 10:37:19 +0200

    Reconstruction moved to chilitags -> much simpler ROS code

--- src/detect.cpp
+++ src/detect.cpp
@@ -1,189 +1,20 @@
-// This file serves as an illustration of how to use cv::solvePnP to estimate the 6D pose of a marker.
-// It requires the chilitags library for 2D tag recognition.
+#include <string>
+#include <iostream>
 
-
-// This header contains the detection part
+// chilitags
 #include <DetectChilitags.hpp>
-// This header provides an easy way to use the tag detection information
-#include <Chilitag.hpp>
+#include <Objects.hpp>
 
-#include <opencv2/core/core_c.h>
-#include <opencv2/calib3d/calib3d.hpp>
-#include <opencv2/highgui/highgui.hpp>
+// opencv2
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp> // for video capture
 
+// ROS
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 
-#include <cstdio>
-#include <iostream>
-#include <vector>
-#include <map>
-
-#include "estimator.h"
-#include "objectconfig.h"
-
-// maximum of objects or tags that can be tracked. Must be =<1024
-#define MAX_OBJECTS 1024
-
 using namespace std;
 using namespace cv;
-
-//  *** For the GUI ***
-const static cv::Scalar scColor(255, 0, 255);
-// These constants will be given to OpenCv for drawing with
-// sub-pixel accuracy with fixed point precision coordinates
-static const int scShift = 16;
-static const float scPrecision = 1<<scShift;
-// *********************
-
-class BaseMarkerBroadcaster {
-
-public:
-    BaseMarkerBroadcaster(InputArray cameraMatrix, InputArray distCoeffs, float gain = 0.9):
-        cameraMatrix(cameraMatrix),
-        distCoeffs(distCoeffs)
-    {
-        // create the estimators with specified gain.
-        for (int i = 0; i < MAX_OBJECTS; ++i) {
-            estimatedTranslations.insert(pair<int, Estimator<Mat>>(i, Estimator<Mat>(gain)));
-            estimatedRotations.insert(pair<int, Estimator<Mat>>(i, Estimator<Mat>(gain)));
-        }
-
-
-    }
-
-    void publish() {
-
-        for (auto& kv : names) {
-
-            setROSTransform(estimatedRotations[kv.first](), 
-                            estimatedTranslations[kv.first](), 
-                            transform);
-
-            br.sendTransform(tf::StampedTransform(
-                            transform, 
-                            ros::Time::now(), 
-                            "camera", 
-                            kv.second));
-        }
-
-
-    }
-
-    void setOutputImage(cv::Mat& image) {
-        outputImage = &image;
-    }
-
-protected:
-    InputArray cameraMatrix;
-    InputArray distCoeffs;
-
-    // store, for each tag ID, an estimator
-    map<int, Estimator<Mat>> estimatedTranslations;
-    map<int, Estimator<Mat>> estimatedRotations;
-
-    map<int, string> names;
-
-    cv::Mat* outputImage;
-
-private:
-    tf::TransformBroadcaster br;
-    tf::Transform transform;
-
-    void setROSTransform(const cv::Mat& rvec, const cv::Mat& tvec, 
-                         tf::Transform& transform)
-    {
-        transform.setOrigin( tf::Vector3( tvec.at<double>(0) / 1000,
-                                        tvec.at<double>(1) / 1000,
-                                        tvec.at<double>(2) / 1000) );
-
-        // the process to get a ROS quaternion out of a OpenCV rotation vector is
-        // not optimal: 
-        // rotation vector -> OpenCV rotation matrix -> ROS matrix -> ROS quaternion...
-        tf::Quaternion qrot;
-        Mat cvmrot;
-        Rodrigues(rvec, cvmrot);
-        tf::Matrix3x3 mrot(
-            cvmrot.at<double>(0), cvmrot.at<double>(1), cvmrot.at<double>(2),
-            cvmrot.at<double>(3), cvmrot.at<double>(4), cvmrot.at<double>(5),
-            cvmrot.at<double>(6), cvmrot.at<double>(7), cvmrot.at<double>(8));
-        mrot.getRotation(qrot);
-        transform.setRotation(qrot);
-    }
-
-};
-
-class MarkerBroadcaster : public BaseMarkerBroadcaster {
-
-public:
-    MarkerBroadcaster(InputArray cameraMatrix, InputArray distCoeffs, float markerSize, float gain = 0.9, bool gui = false): BaseMarkerBroadcaster(cameraMatrix, distCoeffs, gain) {
-
-        hasGUI = gui;
-        markerCorners.push_back(Point3f(0.,0.,0.));
-        markerCorners.push_back(Point3f(markerSize,0.,0.));
-        markerCorners.push_back(Point3f(markerSize,markerSize,0.));
-        markerCorners.push_back(Point3f(0.,markerSize,0.));
-    }
-
-    void process() {
-
-        // We iterate over the 1024 possible tags (from #0 to #1023)
-        for (int tTagId = 0; tTagId < 1024; ++tTagId) {
-
-            chilitags::Chilitag tTag(tTagId);
-
-            if (tTag.isPresent()) {
-
-                names[tTagId] = string("marker_") + to_string(tTagId);
-
-                chilitags::Quad tCorners = tTag.getCorners();
-
-                imagePoints.clear();
-                for (auto p : tCorners.mCorners) {
-                    imagePoints.push_back(p);
-                }
-
-                /****************************************************************
-                *             Pose estimation for each seen marker              *
-                *****************************************************************/
-                
-                // Rotation & translation vectors, computed by cv::solvePnP
-                Mat rvec, tvec;
-
-                // Find the 3D pose of our marker
-                solvePnP(markerCorners, imagePoints, 
-                        cameraMatrix, distCoeffs,
-                        rvec, tvec, false,
-                        cv::ITERATIVE);
-
-                estimatedTranslations[tTagId] << tvec;
-                estimatedRotations[tTagId] << rvec;
-
-                if (hasGUI) draw(imagePoints);
-
-            }
-
-        }
-    }
-
-    void draw(const vector<Point2f>& tCorners) {
-
-        // We draw this quadrilateral
-        for (size_t i = 0; i < 4; ++i) {
-            cv::line(
-                *outputImage,
-                scPrecision*tCorners[i],
-                scPrecision*tCorners[(i+1)%4],
-                scColor, 1, CV_AA, scShift);
-        }
-    }
-
-private:
-    bool hasGUI;
-    vector<Point3f> markerCorners;
-    vector<Point2f> imagePoints;
-
-};
 
 static bool readCameraMatrix(const string& filename,
                              Mat& cameraMatrix, Mat& distCoeffs,
@@ -204,30 +35,37 @@ static bool readCameraMatrix(const string& filename,
     return true;
 }
 
+void setROSTransform(Matx44d trans, tf::Transform& transform)
+{
+    transform.setOrigin( tf::Vector3( trans(0,3) / 1000,
+                                      trans(1,3) / 1000,
+                                      trans(2,3) / 1000) );
+
+    tf::Quaternion qrot;
+    tf::Matrix3x3 mrot(
+        trans(0,0), trans(0,1), trans(0,2),
+        trans(1,0), trans(1,1), trans(1,2),
+        trans(2,0), trans(2,1), trans(2,2));
+    mrot.getRotation(qrot);
+    transform.setRotation(qrot);
+}
+
+
 int main(int argc, char* argv[])
 {
-    const char* help = "Usage: detect [-g|--gui] [--gain <filter gain, default 0.9>] -s <default marker size> [-c <marker config (YAML)>] -i <camera calibration (YAML)>\n";
+    const char* help = "Usage: detect [--gain <filter gain, default 0.9>] [-c <markers configuration (YAML)> | -s <default marker size>] -i <camera calibration (YAML)>\n";
  
-    bool hasGUI = false;
-    bool filtered = true;
-
     // gain = 1 -> no filtering (only measurements)
     float gain = 0.9;
 
     char* intrinsicsFilename = 0;
-    char* markersConfigFilename = 0;
+    string configFilename;
     double squareSize = 0.;
 
     for( int i = 1; i < argc; i++ )
     {
-        if( strcmp(argv[i], "-g") == 0 ||
-            strcmp(argv[i], "--gui") == 0)
-        {
-            cout << "Running with GUI" << endl;
-            hasGUI = true;
-        }
-        else if( strcmp(argv[i], "-c") == 0 )
-            markersConfigFilename = argv[++i];
+        if( strcmp(argv[i], "-c") == 0 )
+            configFilename = string(argv[++i]);
         else if( strcmp(argv[i], "-i") == 0 )
             intrinsicsFilename = argv[++i];
         else if( strcmp(argv[i], "--gain") == 0 )
@@ -253,17 +91,17 @@ int main(int argc, char* argv[])
             }
         }
     }
-    if (intrinsicsFilename == 0 || squareSize == 0)
+    if (intrinsicsFilename == 0 || (configFilename.empty() && squareSize == 0))
     {
         puts(help);
         return 0;
     }
 
-    ObjectConfig objects(markersConfigFilename);
-
     //ROS initialization
     ros::init(argc, argv, "chilitags_tf_broadcaster");
     ros::NodeHandle rosNode;
+    static tf::TransformBroadcaster br;
+    tf::Transform transform;
 
     // For (basic) profiling
     int64 startTickCount;
@@ -275,10 +113,10 @@ int main(int argc, char* argv[])
 
     int tCameraIndex = 0;
 
+    // Read camera calibration
     Mat cameraMatrix, distCoeffs;
     Size calibratedImageSize;
     readCameraMatrix(intrinsicsFilename, cameraMatrix, distCoeffs, calibratedImageSize );
-
 
     // The source of input images
     cv::VideoCapture tCapture(tCameraIndex);
@@ -290,31 +128,23 @@ int main(int argc, char* argv[])
     tCapture.set(CV_CAP_PROP_FRAME_WIDTH, calibratedImageSize.width);
     tCapture.set(CV_CAP_PROP_FRAME_HEIGHT, calibratedImageSize.height);
 
-    if (hasGUI) cv::namedWindow("TagPoseEstimation");
-
-
     // The tag detection happens in the DetectChilitags class.
     // All it needs is a pointer to a OpenCv Image, i.e. a cv::Mat *
     // and a call to its update() method every time the image is updated.
     cv::Mat tInputImage;
-    cv::Mat tOutputImage;
     chilitags::DetectChilitags tDetectChilitags(&tInputImage);
 
-    MarkerBroadcaster markerBroadcaster(cameraMatrix, distCoeffs, 27, gain, hasGUI);
+    // the class chilitags::Objects takes care of 3D localization of the markers
+    // and of objects (counpound markers)
+    chilitags::Objects objects(cameraMatrix, distCoeffs,
+                               configFilename,
+                               squareSize,
+                               gain);
 
     while (rosNode.ok() ) {
 
-        // need to leave some time to OpenCV to display smthg
-        if (hasGUI) cv::waitKey(10);
-
         // Capture a new image.
         tCapture.read(tInputImage);
-
-        if (hasGUI) {
-            // We dont want to draw directly on the input image, so we clone it
-            tOutputImage = tInputImage.clone();
-        }
-
 
         /********************************************************************
          *                      Markers detection                           *
@@ -325,25 +155,33 @@ int main(int argc, char* argv[])
 
         startTickCount = cv::getTickCount();
 
-        markerBroadcaster.process();
-        markerBroadcaster.publish();
+        auto foundObjects = objects.all();
+        
+        cout << "\x1b[K" << foundObjects.size() << " objects found." << endl;
+        /****************************************************************
+        *                Publish TF transforms                          *
+        *****************************************************************/
+        for (auto& kv : foundObjects) {
 
+            setROSTransform(kv.second, 
+                            transform);
 
-
+            br.sendTransform(
+                    tf::StampedTransform(transform, 
+                                         ros::Time::now(), 
+                                         "camera", 
+                                         kv.first));
+        }
 
         poseEstimationDuration = (1000 * (double) (cv::getTickCount()-startTickCount)) / cv::getTickFrequency();
 
         cout << "\x1b[K" 
-            << "Total time: " << poseEstimationDuration + tagRecoDuration << "ms "
-            << "(tag detection: " << tagRecoDuration << "ms, "
-            << ", pose estim. + ROS export: " << poseEstimationDuration << "ms" << endl << "\x1b[1F";
-
-        if (hasGUI) cv::imshow("TagPoseEstimation", tOutputImage);
+            << "Total time: " << setw(5) << poseEstimationDuration + tagRecoDuration << "ms "
+            << "(tag detection: " << setw(5) << tagRecoDuration << "ms, "
+            << ", pose estim. + ROS export: " << setw(5) << poseEstimationDuration << "ms)" << endl << "\x1b[2F";
     }
 
-    cout << endl;
-
-    if (hasGUI) cv::destroyWindow("TagPoseEstimation");
+    cout << endl << endl;
     tCapture.release();
 
     return 0;
